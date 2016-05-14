23d006d7d1c908cbda97119c050ee55224aee274
José Miguel Sánchez <jmi2k@openmailbox.org>
2016-05-14 15:49:41 +0200

    Improve readability and quality

--- src/maybe_type.zig
+++ src/maybe_type.zig
@@ -1,22 +1,22 @@
 pub fn main(args: [][]u8) -> %void {
-    // By design, Zig does not allow null on any type...
-    var i32_sure : i32 = 32;
-
-    // ...except when it is a maybe type:
-    var maybe_x : ?i32 = null;
-    var x_i_guess : ?i32 = 99;
+    /* By design, Zig does not allow null on any type except when it is a maybe
+       type: */
+    var maybe_x : ?i32 = undefined;
+    maybe_x = 137;    // You can assign an i32
+    maybe_x = null;   // Or null
 
     /* Zig provides syntactic mechanisms to ensure code safety: You cannot
        assign directly a maybe type to a normal type, you have to unwrap it */
-    var x_sure : i32 = maybe_x ?? return;     // This would fail and exit
-    var y_sure : i32 = x_i_guess ?? return;   // This would succeed
+    var i32_sure : i32 = undefined;
+    i32_sure = maybe_x ?? return;     // This would fail and exit
+    i32_sure = maybe_x ?? -1;         // This would fail, and assign -1
 
-    /* You can also provide a default value when you find a null */
-    var result : i32 = maybe_x ?? -1;   // result will be -1
+    maybe_x = 20;
+    i32_sure = maybe_x ?? return;   // This would succeed
 
     /* If you are really sure that the maybe value isn't null, you can use the
        unreachable type: */
-    var z_sure : i32 = ??x_i_guess;
+    i32_sure = ??maybe_x;
 
     /* You can also unwrap maybe types with the ?= operator inside an if block.
        It lets you do things such as: */
