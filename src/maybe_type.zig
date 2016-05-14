953d31e2d4a748ab18f46f1195bc1f3ccab13731
José Miguel Sánchez <jmi2k@openmailbox.org>
2016-05-14 13:00:10 +0200

    maybe_type.zig almost finished

--- src/maybe_type.zig
+++ src/maybe_type.zig
@@ -1,6 +1,6 @@
 pub fn main(args: [][]u8) -> %void {
     // By design, Zig does not allow null on any type...
-    var x_sure : i32 = 32;
+    var i32_sure : i32 = 32;
 
     // ...except when it is a maybe type:
     var maybe_x : ?i32 = null;
@@ -8,19 +8,24 @@ pub fn main(args: [][]u8) -> %void {
 
     /* Zig provides syntactic mechanisms to ensure code safety: You cannot
        assign directly a maybe type to a normal type, you have to unwrap it */
-    var y_sure : i32 = maybe_x ?? return;     // This would fail and exit
-    var z_sure : i32 = x_i_guess ?? return;   // This would succeed
+    var x_sure : i32 = maybe_x ?? return;     // This would fail and exit
+    var y_sure : i32 = x_i_guess ?? return;   // This would succeed
 
     /* You can also provide a default value when you find a null */
     var result : i32 = maybe_x ?? -1;   // result will be -1
 
     /* If you are really sure that the maybe value isn't null, you can use the
        unreachable type: */
-    var x_sure : i32 = ??x_i_guess;
+    var z_sure : i32 = ??x_i_guess;
 
     /* You can also unwrap maybe types with the ?= operator inside an if block.
        It lets you do things such as: */
-    // TODO
+    if (const x ?= maybe_x) {
+        /* You only will come here if maybe_x isn't null. And x will be i32,
+           not ?i32, so you can use it directly: */
+
+        var value : i32 = x;
+    }
 
     /* More useful examples:
         src/types.zig
