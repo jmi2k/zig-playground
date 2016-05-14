d6a649da590421882487949367fc30006fffc5bb
José Miguel Sánchez <jmi2k@openmailbox.org>
2016-05-14 13:05:21 +0200

    Start work in unreachable_type.zig

---
+++ src/unreachable_type.zig
@@ -0,0 +1,36 @@
+pub fn main(args: [][]u8) -> %void {
+    /* Zig provides a special type, that it isn't useful by itself, but for
+       what it does when debugging. The unreachable type should be used with
+       great care and responsability: in debug mode it triggers a trap, but in
+       release mode it produces undefined behaviour. When you use it, you have
+       to be absolutely sure the program will never use it! */
+
+    // Let's start with something simple: Using it in an if block:
+    var false_sure : bool = false;
+    if (false_sure) { unreachable{} }   // It will always be, well, unreachable
+
+    /* Special types, such as error and maybe types, can use unreachable when
+       unwrapping. They also provide quick ways of dealing with unreachable: */
+    var no_maybe : ?i32 = 84;
+    var no_error : %i32 = 632;
+
+    var x_sure : i32 = ??no_maybe;   // Short operator, unwraps a maybe type
+    var y_sure : i32 = %%no_error;   // Short operator, unwraps an error type
+
+    // Finally, unreachable as a return type. Guess what it does!
+    /* TODO: Zig is smarter than I'd like. Because of the infinite loop, this
+       won't compile */
+    never_return();
+
+    /* More useful examples:
+        src/types.zig
+        src/maybe_type.zig
+        src/error_type.zig */
+}
+
+fn never_return() -> unreachable {
+    /* This function should never ever return, hence the return type. It will
+       hang the program, so press ctrl-c */
+
+    while (true) {}
+}
