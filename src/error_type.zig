0d34219e337e34126c6f120a0d5b0a03830f9b4d
José Miguel Sánchez <jmi2k@openmailbox.org>
2016-05-14 15:44:29 +0200

    error_type.zig almost finished

--- src/error_type.zig
+++ src/error_type.zig
@@ -4,10 +4,26 @@ error RandomError;
 pub fn main(args: [][]u8) -> %void {
     /* Zig provides a very nice approach to error handling: errors are
        integrated with another type. This way, you are forced to handle errors
-       everytime they can happen, son ignoring them isn't possible. Error types
-       are prefixed with %: */
-       var i32_or_error : %i32 = 828;      // You can assign an i32
-       i32_or_error = error.RandomError;   // Or an error
+       everytime they can happen, son ignoring them isn't possible. */
 
+    // Error types are prefixed with %:
+    var i32_or_error : %i32 = undefined;
+    i32_or_error = 828;                 // You can assign an i32
+    i32_or_error = error.RandomError;   // Or an error
 
+    /* You can't directly assign an error type to a regular type, you have to
+       unwrap it: */
+    var i32_sure : i32 = undefined;
+    i32_sure = i32_or_error %% 2;        // This will fail and assign 2
+    i32_sure = i32_or_error %% return;   // This will fail and exit
+
+    i32_or_error = 981;
+    i32_sure = i32_or_error %% return;   // This will succeed
+
+    /* If you are really sure that the error value isn't null, you can use the
+       unreachable type: */
+    i32_sure = %%i32_or_error;
+
+    /* More useful examples:
+        src/unreachable_type.zig */
 }
