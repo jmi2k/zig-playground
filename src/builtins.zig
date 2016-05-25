45e43bf4baf6abe3ef742d70e3edcec774acae31
José Miguel Sánchez <jmi2k@openmailbox.org>
2016-05-25 16:51:03 +0200

    Finish work at builtins.zig

--- src/builtins.zig
+++ src/builtins.zig
@@ -29,4 +29,39 @@ pub fn main(args: [][]u8) -> %void {
     // @sizeof will give you the size of a type in bytes:
     %%std.io.stdout.print_u64(@sizeof(u64));
     %%std.io.stdout.printf(" is the size of 'u64' in bytes.\n");
+
+    /* With @compile_var, you can vary your code based on specific parameters:
+       target platform, type of build or, in the future, custom variables: */
+    switch (@compile_var("os")) {
+        macosx => %%std.io.stdout.printf("You're on OS X!\n"),
+        freebsd => %%std.io.stdout.printf("You're on FreeBSD!\n"),
+        linux => %%std.io.stdout.printf("You're on Linux!\n"),
+        else => %%std.io.stdout.printf("You're on a weird OS...\n"),
+    };
+
+    switch (@compile_var("arch")) {
+        armv7 => %%std.io.stdout.printf("You're on a armv7!\n"),
+        i386 => %%std.io.stdout.printf("You're on i386!\n"),
+        x86_64 => %%std.io.stdout.printf("You're on x86_64!\n"),
+        else => %%std.io.stdout.printf("You have a weird arch\n"),
+    };
+
+    // @err_name will return the string representation of an error
+    %%std.io.stdout.printf(@err_name(error.SigInterrupt));
+    %%std.io.stdout.printf("\n");
+
+    // @max_value and @min_value will give you the limits of an integer type
+    %%std.io.stdout.print_u64(@max_value(u64));
+    %%std.io.stdout.printf(" is the max value of an u64 (in your machine).\n");
+
+    %%std.io.stdout.print_u64(@min_value(u64));
+    %%std.io.stdout.printf(" is the min value of an u64 (in your machine).\n");
+
+    /* There are loads of builtins, and only the basic ones are covered here.
+       If you want to get into the most specific ones, take a look at
+       doc/langref.md in the Zig repository */
+
+    /* More useful examples:
+        src/unreachable_type.zig
+        src/error_type.zig */
 }
